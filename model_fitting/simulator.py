f035c8f93b2c295b978b32fe00534ed6ce3c6ffc
patricklawton <plawt001@ucr.edu>
2024-03-21 11:34:38 -0700

    success with extirpation masking
    - Need to address sigm_m likelihood being maximized at upper prior bound, posterior for sigm_m disappears if I increase prior upper bound

--- model_fitting/simulator.py
+++ model_fitting/simulator.py
@@ -27,7 +27,7 @@ def simulator(params):
     densities_o = pd.read_csv(fn, header=None)
     densities_o[0] = [round(v) for v in densities_o[0]]
     N_0_1 = densities_o[densities_o[0] == 1][1].to_numpy()
-    N_vec = np.zeros((len(N_0_1), len(t_vec)))
+    N_vec = np.ma.array(np.zeros((len(N_0_1), len(t_vec))))
     N_vec[:,0] = N_0_1
     N_vec = N_vec.astype(int)
     census_init = N_vec.sum(axis=1)
@@ -37,12 +37,11 @@ def simulator(params):
     K_a = K_seedling * np.exp(-kappa*t_vec) + K_adult
     nu_a = alph_nu * np.exp(-beta_nu*t_vec) + gamm_nu
 
-    extirpated_pops = 0
-    census_extirpated_pop_i = []
-    census_pop_i = np.arange(N_vec.shape[0])
     for age_i, t in enumerate(t_vec[:-1]):
-        extirpated_pop_i = []
         for pop_i, N_pop in enumerate(N_vec):
+            # If pop already extirpated, skip
+            if (np.ma.is_masked(N_vec)) and (np.ma.getmask(N_vec)[pop_i, 0]):
+                continue
             # Add density dependent term to mortalities
             dens_dep = ((nu_a)*(1-m_a)) / (1 + np.exp(-eta*K_adult*(np.sum(N_pop/K_a) - 1)))
             m_a_N = m_a + dens_dep
@@ -55,43 +54,27 @@ def simulator(params):
             N_vec[pop_i] = num_survivors
             # Note if population was extirpated
             if np.sum(num_survivors) == 0:
-                #print('pop extirpated')
-                extirpated_pop_i.append(pop_i)
-                census_extirpated_pop_i.append(pop_i)
-                extirpated_pops += 1
-        # Delete extirpated populations before computing mortalities
-        N_vec = np.delete(N_vec, extirpated_pop_i, axis=0)
+                #print('pop {} extirpated'.format(pop_i))
+                N_vec[pop_i, :] = np.ma.masked
+                census_init[pop_i] = np.ma.masked
+                #print(N_vec)
         # If enough populations extirpated, consider parameter set invalid
-        if extirpated_pops > 3:
+        if (np.ma.is_masked(N_vec)) and (sum(np.ma.getmask(N_vec)[:,0]) > 3):
             #print('invalid sim')
             results[0:res_len] = np.ones(len(census_yrs)-1)*10
             results[res_len:res_len*2] = np.ones(len(census_yrs)-1)*-1
             results[res_len*2:res_len*3] = np.ones(len(census_yrs)-1)*20
             break
         elif t+1 in census_yrs:
-            # Delete extirpated pops since last census
-            census_init = np.delete(census_init, census_extirpated_pop_i)
             # Calculate and store mortality stats
             delta_t = (t+1) - census_yr_init
             census_final = N_vec.sum(axis=1)
-            try:
-                mortality = ((census_init - census_final) / census_init) / delta_t
-            except:
-                #print(params)
-                #print(census_extirpated_pop_i)
-                #print('time', t, 'delta_t', delta_t)
-                #mortality = ((census_init - census_final) / census_init) / delta_t
-                results[0:res_len] = np.ones(len(census_yrs)-1)*10
-                results[res_len:res_len*2] = np.ones(len(census_yrs)-1)*-1
-                results[res_len*2:res_len*3] = np.ones(len(census_yrs)-1)*20
-                break
-            #mortality = ((census_init - census_final) / census_init) / delta_t
+            mortality = ((census_init - census_final) / census_init) / delta_t
             results[res_i] = np.mean(mortality)
             results[res_len + res_i] = np.min(mortality)
             results[res_len*2 + res_i] = np.max(mortality)
             # Reset for next census
             res_i += 1
             census_init = census_final
-            census_extirpated_pop_i = []
             census_yr_init = t+1
     return results
