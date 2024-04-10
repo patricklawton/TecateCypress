74e7e014f6844a214a000457d90696cab1b38b54
patricklawton <plawt001@ucr.edu>
2024-04-09 17:43:03 -0700

    Fecundity inference attempt

---
+++ model_fitting/fecundity/simulator.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+# Brennan 2019
+num_sites = np.array([2,4,4,4,2])
+stand_age_bren = np.array([7,24,33,42,53])
+prefire_density = np.array([2205,48,3430,233,400])
+prefire_density_se = np.array([35,14,308,136,240])
+prefire_density_sd = prefire_density_se * np.sqrt(num_sites)
+rec_density = np.array([0,10000,18500,29000,182000])
+rec_density_se = np.array([0,2345,9385,18443,92000])
+rec_density_sd = rec_density_se * np.sqrt(num_sites)
+fecundity_bren = rec_density / prefire_density
+fecundity_err_bren = np.zeros(len(fecundity_bren))
+for i in range(len(fecundity_err_bren)):
+    if (prefire_density[i] == 0) or (rec_density[i] == 0):
+        fecundity_err = 0
+    else:
+        # fecundity_err = fecundity_bren[i] * np.sqrt((prefire_density_se[i]/prefire_density[i])**2 + (rec_density_se[i]/rec_density[i])**2)
+        fecundity_err = fecundity_bren[i] * np.sqrt((prefire_density_sd[i]/prefire_density[i])**2 + (rec_density_sd[i]/rec_density[i])**2)
+    fecundity_err_bren[i] = fecundity_err
+
+# Dunn 1986
+stand_age_dunn = np.array([10,19,20,20,30,36,39,63])
+fecundity_dunn = (np.array([0,0.1,2.9,26.5,15.7,1206.5,1387.3,1400]) / 100) + 1
+# Assume something ad hoc for CV on Dunn's measurements
+fecundity_err_dunn = np.repeat(0.75, len(fecundity_dunn)) * fecundity_dunn
+
+stand_age_all = np.concatenate((stand_age_bren, stand_age_dunn))
+sort = np.argsort(stand_age_all)
+stand_age_all = stand_age_all[sort]
+fecundity_all = np.concatenate((fecundity_bren, fecundity_dunn))[sort]
+fecundity_err_all = np.concatenate((fecundity_err_bren,fecundity_err_dunn))[sort]
+weights = np.concatenate((num_sites, np.ones(len(stand_age_dunn))))[sort]
+binwidth = 22
+numbins = max(stand_age_all) // binwidth + (max(stand_age_all) % binwidth > 0)
+age_cntrs = np.zeros(numbins)
+mean_fecundities = []
+bin_errors = []
+for i in range(numbins):
+    filt = ((stand_age_all > binwidth*i) & (stand_age_all <= binwidth*(i+1)))
+    avg = np.average(fecundity_all[filt], weights=weights[filt])
+    mean_fecundities.append(avg)
+    age_cntr = binwidth*i + binwidth/2
+    age_cntrs[i] = age_cntr
+    err = (1/sum(weights[filt]))*np.sqrt(sum((fecundity_err_all[filt]*weights[filt])**2))
+    bin_errors.append(err)
+
+def save_observations():
+    observations = np.concatenate((mean_fecundities, bin_errors))
+    np.save('fecundity/observations/observations.npy', observations)
+
+def simulator(params):
+    rho_max = params[0]; eta_rho = params[1]; a_mature = params[2]
+    sigm_max = params[3]; eta_sigm = params[4]; a_sigm_star = params[5]
+
+    # Read this in from file(s) in the actual script
+    a_vec = np.concatenate((np.repeat(stand_age_bren, num_sites), stand_age_dunn))
+    a_vec.sort()
+
+    rho_a = rho_max / (1+np.exp(-eta_rho*(a_vec-a_mature)))
+    rng = np.random.default_rng()
+    sigm_a = sigm_max / (1+np.exp(-eta_sigm*(a_vec-a_sigm_star)))
+    epsilon_rho = rng.lognormal(np.zeros_like(a_vec), sigm_a)
+    fecundities = rng.poisson(rho_a*epsilon_rho)
+    # fecundities = rng.poisson(rho_a)
+
+    mean_fecundities = np.zeros(numbins)
+    bin_stdevs = np.zeros(numbins)
+    results = np.empty(numbins*2)
+    for i in range(numbins):
+        filt = ((a_vec > binwidth*i) & (a_vec <= binwidth*(i+1)))
+        fecundities_bin = fecundities[filt]
+        mean_fecundities[i] = fecundities_bin.mean()
+        bin_stdevs[i] = np.std(fecundities_bin, ddof=1)
+
+    results[0:numbins] = mean_fecundities
+    results[numbins:numbins*2] = bin_stdevs
+    return results
--- model_fitting/sbi_main.py
+++ model_fitting/sbi_main.py
@@ -11,106 +11,132 @@ from torch import tensor
 import pickle
 import pandas as pd
 import os
-from simulator import simulator
+#from simulator import simulator
 from scipy.stats import moment
 
-overwrite_observations = False
-overwrite_simulations = False
-add_simulations = True
-overwrite_posterior = False
+overwrite_observations = True
+overwrite_simulations = True
+add_simulations = False
+overwrite_posterior = True
 
-if (os.path.isfile('observations/observations.npy') == False) or overwrite_observations:
+processes = ['fecundity']
+for pr in processes:
+    if pr == 'mortality':
+        from mortality.simulator import simulator
+        labels = ['alph_m', 'beta_m', 'sigm_m','alph_nu']
+        ranges = np.array([
+                           # alph_m
+                           [0.01, 0.6], 
+                           # beta_m
+                           [0.01, 0.9], 
+                           # sigm_m
+                           [0.1,1.7], 
+                           # alph_nu
+                           [0.01,2.]
+        ])
+        restrictor_sims = 50_000
+        training_sims = 150_000
+        num_samples = 1_000_000 
+    elif pr == 'fecundity':
+        from fecundity.simulator import simulator, save_observations
+        labels = ['rho_max', 'eta_rho', 'a_mature', 'sigm_max', 'eta_sigm', 'a_sigm_star']
+        ranges = np.array([
+                           [100, 600],
+                           [0.01, 0.8],
+                           [15, 80],
+                           [0.01, 5],
+                           [0.01, 0.8],
+                           [15, 80]
+        ])
+        restrictor_sims = 5_000
+        training_sims = 5_000
+        num_samples = 1_000_000 
+
     # First, compute and store summary statistics of observed data
-    census_yrs = [2,6,8,11,14]
-    fn = 'observations/mortality.csv'
-    mortality_o = pd.read_csv(fn, header=None)
-    mortality_o[0] = [round(v) for v in mortality_o[0]]
-    # Use the first 3 moments
-    m1 = []; m2 = []; m3 = []
-    for t_i, t in enumerate(census_yrs):
-        mort_sub = mortality_o[mortality_o[0]==t][1].to_numpy()
-        est_mean = np.mean(mort_sub)
-        m1.append(est_mean)
-        m2.append(moment(mort_sub, moment=2))
-        m3.append(moment(mort_sub, moment=3))
-    observations = np.concatenate((m1,m2,m3))
-    np.save('observations/observations.npy', observations)
-x_o = np.load('observations/observations.npy')
-x_o = torch.Tensor(x_o)
+    fn = pr + '/observations/observations.npy'
+    if ((not os.path.isfile(fn)) or overwrite_observations) and (pr=='mortality'):
+        census_yrs = [2,6,8,11,14]
+        fn = 'observations/mortality.csv'
+        mortality_o = pd.read_csv(fn, header=None)
+        mortality_o[0] = [round(v) for v in mortality_o[0]]
+        # Use the first 3 moments
+        m1 = []; m2 = []; m3 = []
+        for t_i, t in enumerate(census_yrs):
+            mort_sub = mortality_o[mortality_o[0]==t][1].to_numpy()
+            est_mean = np.mean(mort_sub)
+            m1.append(est_mean)
+            m2.append(moment(mort_sub, moment=2))
+            m3.append(moment(mort_sub, moment=3))
+        observations = np.concatenate((m1,m2,m3))
+        np.save(fn, observations)
+    elif ((not os.path.isfile(fn)) or overwrite_observations) and (pr=='fecundity'):
+        save_observations()
+    x_o = np.load(fn)
+    x_o = torch.Tensor(x_o)
+    print(x_o)
 
-ranges = np.array([
-                   # alph_m
-                   [0.01, 0.6], 
-                   # beta_m
-                   [0.01, 0.9], 
-                   # sigm_m
-                   [0.1,1.7], 
-                   # alph_nu
-                   [0.01,2.]
-])
-if (os.path.isfile('all_theta.pkl') == False) or overwrite_simulations:
-    priors = [Uniform(tensor([rng[0]]), tensor([rng[1]])) for rng in ranges]
-    prior = MultipleIndependent(priors)
-    prior, theta_numel, prior_returns_numpy = utils.user_input_checks.process_prior(prior)
-    with open("prior.pkl", "wb") as handle:
-        pickle.dump(prior, handle)
-    simulator = utils.user_input_checks.process_simulator(simulator, prior, is_numpy_simulator=True)
-    theta, x = simulate_for_sbi(simulator, proposal=prior, num_simulations=50000, num_workers=8)
-    restriction_estimator = RestrictionEstimator(prior=prior)
-    restriction_estimator.append_simulations(theta, x)
-    classifier = restriction_estimator.train()
-    restricted_prior = restriction_estimator.restrict_prior()
-    with open("restricted_prior.pkl", "wb") as handle:
-        pickle.dump(restricted_prior, handle)
-    new_theta, new_x = simulate_for_sbi(simulator, restricted_prior, 150000)
-    restriction_estimator.append_simulations(
-        new_theta, new_x
-    )  # Gather the new simulations in the `restriction_estimator`.
-    (
-        all_theta,
-        all_x,
-        _,
-    ) = restriction_estimator.get_simulations()  # Get all simulations run so far.
-    with open("all_theta.pkl", "wb") as handle:
-        pickle.dump(all_theta, handle)
-    with open("all_x.pkl", "wb") as handle:
-        pickle.dump(all_x, handle)
-else:
-    with open("prior.pkl", "rb") as handle:
-        prior = pickle.load(handle)
-    with open("restricted_prior.pkl", "rb") as handle:
-        restricted_prior = pickle.load(handle)
-    with open("all_theta.pkl", "rb") as handle:
-        all_theta = pickle.load(handle)
-    with open("all_x.pkl", "rb") as handle:
-        all_x = pickle.load(handle)
-    if add_simulations:
+    if (not os.path.isfile(pr+'/all_theta.pkl')) or overwrite_simulations:
+        priors = [Uniform(tensor([rng[0]]), tensor([rng[1]])) for rng in ranges]
+        prior = MultipleIndependent(priors)
+        prior, theta_numel, prior_returns_numpy = utils.user_input_checks.process_prior(prior)
+        with open(pr+"/prior.pkl", "wb") as handle:
+            pickle.dump(prior, handle)
         simulator = utils.user_input_checks.process_simulator(simulator, prior, is_numpy_simulator=True)
-        new_theta, new_x = simulate_for_sbi(simulator, restricted_prior, 100_000, num_workers=8)
-        all_theta = torch.cat((all_theta, new_theta), 0)
-        all_x = torch.cat((all_x, new_x), 0)
-        with open("all_theta.pkl", "wb") as handle:
+        theta, x = simulate_for_sbi(simulator, proposal=prior, num_simulations=restrictor_sims, num_workers=8)
+        print(x[0])
+        restriction_estimator = RestrictionEstimator(prior=prior)
+        restriction_estimator.append_simulations(theta, x)
+        classifier = restriction_estimator.train()
+        restricted_prior = restriction_estimator.restrict_prior()
+        with open(pr+"/restricted_prior.pkl", "wb") as handle:
+            pickle.dump(restricted_prior, handle)
+        new_theta, new_x = simulate_for_sbi(simulator, restricted_prior, training_sims)
+        restriction_estimator.append_simulations(
+            new_theta, new_x
+        )  # Gather the new simulations in the `restriction_estimator`.
+        (
+            all_theta,
+            all_x,
+            _,
+        ) = restriction_estimator.get_simulations()  # Get all simulations run so far.
+        with open(pr+"/all_theta.pkl", "wb") as handle:
             pickle.dump(all_theta, handle)
-        with open("all_x.pkl", "wb") as handle:
+        with open(pr+"/all_x.pkl", "wb") as handle:
             pickle.dump(all_x, handle)
+    else:
+        with open(pr+"/prior.pkl", "rb") as handle:
+            prior = pickle.load(handle)
+        with open(pr+"/restricted_prior.pkl", "rb") as handle:
+            restricted_prior = pickle.load(handle)
+        with open(pr+"/all_theta.pkl", "rb") as handle:
+            all_theta = pickle.load(handle)
+        with open(pr+"/all_x.pkl", "rb") as handle:
+            all_x = pickle.load(handle)
+        if add_simulations:
+            simulator = utils.user_input_checks.process_simulator(simulator, prior, is_numpy_simulator=True)
+            new_theta, new_x = simulate_for_sbi(simulator, restricted_prior, 100_000, num_workers=8)
+            all_theta = torch.cat((all_theta, new_theta), 0)
+            all_x = torch.cat((all_x, new_x), 0)
+            with open(pr+"/all_theta.pkl", "wb") as handle:
+                pickle.dump(all_theta, handle)
+            with open(pr+"/all_x.pkl", "wb") as handle:
+                pickle.dump(all_x, handle)
 
-if (os.path.isfile('posterior.pkl') == False) or overwrite_posterior or add_simulations:
-    inferer = SNPE(prior, show_progress_bars=True, density_estimator="mdn")
-    inferer = inferer.append_simulations(all_theta, all_x)
-    density_estimator = inferer.train()
-    posterior = inferer.build_posterior(density_estimator)
-    posterior.set_default_x(x_o)
-    with open("posterior.pkl", "wb") as handle:
-        pickle.dump(posterior, handle)
-else:
-    with open("posterior.pkl", "rb") as handle:
-        posterior = pickle.load(handle)
+    if (not os.path.isfile(pr+'/posterior.pkl')) or overwrite_posterior or add_simulations:
+        inferer = SNPE(prior, show_progress_bars=True, density_estimator="mdn")
+        inferer = inferer.append_simulations(all_theta, all_x)
+        density_estimator = inferer.train()
+        posterior = inferer.build_posterior(density_estimator)
+        posterior.set_default_x(x_o)
+        with open(pr+"/posterior.pkl", "wb") as handle:
+            pickle.dump(posterior, handle)
+    else:
+        with open(pr+"/posterior.pkl", "rb") as handle:
+            posterior = pickle.load(handle)
 
-num_samples = 1_000_000
-npe_samples = posterior.sample(sample_shape=(num_samples,))
-torch.save(npe_samples, 'posterior_samples.pkl')
-labels = ['alph_m', 'beta_m', 'sigm_m','alph_nu']
-_ = analysis.pairplot(
-    npe_samples, limits=ranges, figsize=(10, 10), labels=labels
-)
-_[0].savefig('sbi_figs/posterior_pairplot.png', bbox_inches='tight')
+    npe_samples = posterior.sample(sample_shape=(num_samples,))
+    torch.save(npe_samples, pr+'/posterior_samples.pkl')
+    _ = analysis.pairplot(
+        npe_samples, limits=ranges, figsize=(10, 10), labels=labels
+    )
+    _[0].savefig('sbi_figs/{}_posterior_pairplot.png'.format(pr), bbox_inches='tight')
