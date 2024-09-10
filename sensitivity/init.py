65a4914503b335b1b837557ede02a9f3733ec5ca
patricklawton <plawt001@ucr.edu>
2024-09-10 14:44:16 -0700

    Add uncertainty to expected fri values

--- sensitivity/init.py
+++ sensitivity/init.py
@@ -21,9 +21,9 @@ if not os.path.isfile(sd_fn):
 A_cell = 270**2 / 10_000 #Ha
 sdm_min = 0.32649827003479004 
 #Aeff_vec = np.array([np.round(A_cell*sdm_min, 2)])
+#Aeff_vec = np.array([np.round(A_cell, 2)])
 Aeff_vec = np.array([np.round(A_cell, 2)])
-Aeff_vec = np.array([np.round(A_cell, 2)])
-t_final_vec = np.array([500])
+t_final_vec = np.array([600])
 demographic_samples_vec = np.array([500])
 method_vec = ["nint"]
 #for Aeff, t_final, demographic_samples in zip(Aeff_vec, t_final_vec, demographic_samples_vec):
