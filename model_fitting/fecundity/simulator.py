821d7b502ff5aa08e7524c935c5e4738f262bcaa
patricklawton <plawt001@ucr.edu>
2025-02-12 16:00:07 -0800

    Add in other data sources

--- model_fitting/fecundity/simulator.py
+++ model_fitting/fecundity/simulator.py
@@ -16,9 +16,11 @@ for i in range(len(fecundity_err_bren)):
     if (prefire_density[i] == 0) or (rec_density[i] == 0):
         fecundity_err = 0
     else:
-        #fecundity_err = fecundity_bren[i] * np.sqrt((prefire_density_se[i]/prefire_density[i])**2 + (rec_density_se[i]/rec_density[i])**2)
         fecundity_err = fecundity_bren[i] * np.sqrt((prefire_density_sd[i]/prefire_density[i])**2 + (rec_density_sd[i]/rec_density[i])**2)
     fecundity_err_bren[i] = fecundity_err
+stand_age_bren = np.delete(stand_age_bren, 1)
+fecundity_bren = np.delete(fecundity_bren, 1)
+num_sites = np.delete(num_sites, 1)
 
 # Dunn 1986
 stand_age_dunn = np.array([10,19,20,20,30,36,39,63])
@@ -26,22 +28,37 @@ fecundity_dunn = (np.array([0,0.1,2.9,26.5,15.7,1206.5,1387.3,1400]) / 100) + 1
 # Assume something ad hoc for CV on Dunn's measurements
 fecundity_err_dunn = np.repeat(0.75, len(fecundity_dunn)) * fecundity_dunn
 
-stand_age_all = np.concatenate((stand_age_bren, stand_age_dunn))
+# RodriguezBuritanica 2013
+stand_age_rb = np.array([14.379, 23.308, 31.184, 35.033, 41.803, 43.438, 46.303]) #Using the reported 'average age' as an approximation
+seed_production_rb = np.array([0, 0, 6.288, 33.853, 105.329, 290.834, 605.736]) #Annual
+fecundity_rb = seed_production_rb * 0.1 #They assume a 10% germination rate; 0.1 used as transition rate to 'seedling' stage, which has a range of ages
+                                        #but given high survival in this stage and use of the term "germination", ok to approximate this way
+
+# deGouvenian 2006
+stand_age_dg = np.array([38, 52, 43, 46])
+seed_production_dg = np.array([2937, 41200, 5340+13350, 3000+5625]) / stand_age_dg
+fecundity_dg = seed_production_dg * 0.1
+
+stand_age_all = np.concatenate((stand_age_bren, stand_age_dunn, stand_age_rb, stand_age_dg))
 sort = np.argsort(stand_age_all)
 stand_age_all = stand_age_all[sort]
-fecundity_all = np.concatenate((fecundity_bren, fecundity_dunn))[sort]
-fecundity_err_all = np.concatenate((fecundity_err_bren,fecundity_err_dunn))[sort]
-weights = np.concatenate((num_sites, np.ones(len(stand_age_dunn))))[sort]
+fecundity_all = np.concatenate((fecundity_bren, fecundity_dunn, fecundity_rb, fecundity_dg))[sort]
+#fecundity_err_all = np.concatenate((fecundity_err_bren,fecundity_err_dunn))[sort]
+weights = np.concatenate((num_sites, np.ones(len(stand_age_dunn)), np.ones(len(stand_age_rb)), np.ones(len(stand_age_dg))))[sort]
 binwidth = 22
 numbins = max(stand_age_all) // binwidth + (max(stand_age_all) % binwidth > 0)
+custom_bin_edges = [0, 30, 40, 50, 65]
+numbins = len(custom_bin_edges) - 1
 age_cntrs = np.zeros(numbins)
 mean_fecundities = []
 bin_errors = []
 for i in range(numbins):
-    filt = ((stand_age_all > binwidth*i) & (stand_age_all <= binwidth*(i+1)))
+    #filt = ((stand_age_all > binwidth*i) & (stand_age_all <= binwidth*(i+1)))
+    filt = (stand_age_all > custom_bin_edges[i]) & (stand_age_all <= custom_bin_edges[i+1])
     avg = np.average(fecundity_all[filt], weights=weights[filt])
     mean_fecundities.append(avg)
-    age_cntr = binwidth*i + binwidth/2
+    #age_cntr = binwidth*i + binwidth/2
+    age_cntr = (custom_bin_edges[i+1] - custom_bin_edges[i])/2 + custom_bin_edges[i]
     age_cntrs[i] = age_cntr
     #err = (1/sum(weights[filt]))*np.sqrt(sum((fecundity_err_all[filt]*weights[filt])**2))
     '''Just use std from averages in bin'''
@@ -65,6 +82,8 @@ def simulator(params):
     a_vec = np.concatenate((
                             np.repeat(stand_age_bren, num_sites), 
                             stand_age_dunn,
+                            stand_age_rb,
+                            stand_age_dg,
                             [numbins*binwidth + 1, (numbins+1)*binwidth]
                            ))
     a_vec.sort()
@@ -73,7 +92,7 @@ def simulator(params):
     rng = np.random.default_rng()
     #sigm_a = sigm_max / (1+np.exp(-eta_sigm*(a_vec-a_sigm_star)))
     sigm_a = np.repeat(sigm_max, len(a_vec))
-    a_star = a_mature - (np.log((1/0.99)-1) / eta_rho) # Age where we want env stoch to kick in
+    a_star = a_mature - (np.log((1/0.90)-1) / eta_rho) # Age where we want env stoch to kick in
     sigm_a[a_vec < a_star] = 0.0
     epsilon_rho = rng.lognormal(np.zeros_like(a_vec), sigm_a)
     fecundities = rng.poisson(rho_a*epsilon_rho)
@@ -82,7 +101,8 @@ def simulator(params):
     bin_stdevs = np.zeros(numbins)
     results = np.empty(numbins*2)
     for i in range(numbins):
-        filt = ((a_vec > binwidth*i) & (a_vec <= binwidth*(i+1)))
+        #filt = ((a_vec > binwidth*i) & (a_vec <= binwidth*(i+1)))
+        filt = (a_vec > custom_bin_edges[i]) & (a_vec <= custom_bin_edges[i+1])
         fecundities_bin = fecundities[filt]
         mean_fecundities[i] = fecundities_bin.mean()
         bin_stdevs[i] = np.std(fecundities_bin, ddof=1)
